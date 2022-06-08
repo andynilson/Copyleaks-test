735b6161a02a944ed1d1619525b4d0d6117c34f5
Andy Nilson <andy@andynilson.com>
2022-06-08 08:25:35 -0700

    added functionality to loop through all files in solutions directory

--- sampleMain.py
+++ sampleMain.py
@@ -4,7 +4,9 @@ from copyleaks.copyleaks import Copyleaks, Products
 from copyleaks.exceptions.command_error import CommandError
 from copyleaks.models.submit.document import FileDocument
 from copyleaks.models.submit.properties.scan_properties import ScanProperties
+import os
 
+directory = 'solutions'
 
 # email address and api key
 EMAIL_ADDRESS = 'andy.n@turing.com'
@@ -22,19 +24,24 @@ except CommandError as ce:
 print("Logged successfully!\nToken:")
 print(auth_token)
 
-
-# This example is going to scan a file for plagiarism.
-print("Submitting a new file...")
-file_text = open('solutions/baseWhileLoopSolution.py', 'rb')
-file_read = file_text.read()
-BASE64_FILE_CONTENT = result = base64.b64encode(file_read).decode('ascii') # convert the file to base 64 format
-FILENAME = "baseWhileLoopSolution.py"
-scan_id = random.randint(100, 100000)  # generate a random scan id
-file_submission = FileDocument(BASE64_FILE_CONTENT, FILENAME)
-# Send test results to pipedream webhook
-scan_properties = ScanProperties('https://eojs8lo649wa4pk.m.pipedream.net')
-scan_properties.set_sandbox(True)
-file_submission.set_properties(scan_properties)
-Copyleaks.submit_file(PRODUCT, auth_token, scan_id, file_submission)  # sending the submission to scanning
-print("Send to scanning")
-print("You will be notified, using your webhook, once the scan was completed.")
+for filename in os.listdir(directory):
+    f = os.path.join(filename)
+    df = os.path.join(directory, filename)
+    # checking if it is a file
+    if os.path.isfile(df):
+        print(df)
+        # This example is going to scan a file for plagiarism.
+        print("Submitting a new file...")
+        file_text = open(df, 'rb')
+        file_read = file_text.read()
+        BASE64_FILE_CONTENT = result = base64.b64encode(file_read).decode('ascii') # convert the file to base 64 format
+        FILENAME = str(f)
+        scan_id = random.randint(100, 100000)  # generate a random scan id
+        file_submission = FileDocument(BASE64_FILE_CONTENT, FILENAME)
+        # Send test results to pipedream webhook
+        scan_properties = ScanProperties('https://eojs8lo649wa4pk.m.pipedream.net')
+        scan_properties.set_sandbox(True)
+        file_submission.set_properties(scan_properties)
+        Copyleaks.submit_file(PRODUCT, auth_token, scan_id, file_submission)  # sending the submission to scanning
+        print("Send to scanning")
+        print("You will be notified, using your webhook, once the scan was completed.")
